6b21210bec6dc0b08a91a72983093ad6f8d56e8c
Keith Funkhouser <kfunkhouser@cnm.edu>
2020-05-09 11:32:02 -0600

    Add insert/update/delete/select statements for created tables

--- sql/data-design.sql
+++ sql/data-design.sql
@@ -56,3 +56,46 @@ CREATE TABLE `share` (
 	FOREIGN KEY(shareFolderId) REFERENCES folder(folderId),
 	PRIMARY KEY(shareUserId, shareFolderId)
 );
+
+INSERT INTO `user` VALUES (
+    UNHEX('b7e9cbdfde6d44b9b2a0abd0a3898a4f'),
+    'VioWJmEzi50MeDoZxTUWmvcLGxUocLYI',
+    'sally@sallydesign.com',
+    'YdWEUYtLZP5McRTmreFJd57df6BtMcBYNsOnBChjNgH0ptQSewNRqAj5WNAvrdSdHVkb6fDOt9Fe3K8AylGk4KX6YNDCg82CT',
+    'Sally',
+    'Admin'
+);
+
+INSERT INTO `user` VALUES (
+    UNHEX('ac8e277e8d9b4e17aa60209e8f88b846'),
+    'Oe6K3ifCEMqPKOmO0L6iFjZ8drZD6REw',
+    'john@sallydesign.com',
+    'JnY2XeHtOovKlQJ96rfy4Mo2TxDnz86d7wmltug7VQHgtp14PAxlvDZGQjipo1VdGBYYokySJYqrBKT9tkjopCgwfY7UxiAuZ',
+    'John',
+    'Team Member'
+);
+
+INSERT INTO `user` VALUES (
+    UNHEX('1cc4c7b658cf419a8dcba66d4583bb4d'),
+    '5NOMiQZ4u7scANQIY90Zc5F6E7RPldzh',
+    'sarah@sallydesign.com',
+    '0lDBpKZPUgHOemxYIZSKXw4VtwTszQpW6JLZHCOvzaM8I84Djc1YibX0d113vesgbYIt45XsAQeDNKBywiXUUrG3WcjsUgARS',
+    'Sara',
+    'Team Member'
+);
+
+UPDATE `user` SET userName = 'Sarah' WHERE userId = UNHEX('1cc4c7b658cf419a8dcba66d4583bb4d');
+
+DELETE FROM `user` WHERE userId = UNHEX('ac8e277e8d9b4e17aa60209e8f88b846');
+
+INSERT INTO folder(folderID, folderUserId, folderDateCreated, folderName, folderPath) VALUES (
+    UNHEX('f81e0992154b4cf1813f8d03038f4c28'),
+    UNHEX('b7e9cbdfde6d44b9b2a0abd0a3898a4f'),
+    '2020-05-09 11:05:00.000000',
+    'renders',
+    'home/clients/initech/logo'
+);
+
+SELECT * FROM `user` WHERE userId = UNHEX('1cc4c7b658cf419a8dcba66d4583bb4d');
+
+SELECT userName FROM folder INNER JOIN `user` ON folder.folderUserId = `user`.userId WHERE folderName = 'renders';
